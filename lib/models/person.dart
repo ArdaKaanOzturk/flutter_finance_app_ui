43289c544b675ba20ef5b04a46f15c43f3231107
ArdaKaanOzturk <ardakaan.ozturk@live.com>
2024-05-30 19:29:07 +0300

    ui submit

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -1,32 +1,29 @@
-import 'dart:ui';
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class User{
-  final String id;
+class Person {
   final String name;
-  final String urlAvatar;
-  final String price;
-  final String time;
+  final double amount;
 
-  const User({
-    required this.id,
-    required this.name,
-    required this.urlAvatar,
-    required this.price,
-    required this.time,
-  });
-  static User fromJson(json) => User(id: json['id'], name: json['name'], urlAvatar: json['urlAvatar'], price: json['price'], time: json['time']);
+  Person({required this.name, required this.amount});
+
+  factory Person.fromJson(Map<String, dynamic> json) {
+    // Fake API'den gelen verilerde amount alanı olmadığı için rastgele bir amount değeri oluşturuyoruz
+    return Person(
+      name: json['name'],
+      amount: ((json['id'] * 100).toDouble() * (json['id'] % 2 == 0 ? 1 : -1)),
+    );
+  }
 }
 
-List<User> users = getUser();
+Future<List<Person>> fetchPersons() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
 
-static List<User> getUser(){
-
-  const data = [
-    {"id": "1", "name": 'Mona OGarro', "urlAvatar": "https://randomuser.me/api/portraits/men/79.jpg","price": "\$300", "time": "11:27 AM"},
-  ];
-
-  return data.map<User>(User.fromJson).toList();
-
+  if (response.statusCode == 200) {
+    List<dynamic> body = json.decode(response.body);
+    List<Person> persons = body.map((dynamic item) => Person.fromJson(item)).toList();
+    return persons;
+  } else {
+    throw Exception('Failed to load persons');
+  }
 }
-
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,47 +1,180 @@
-import 'package:dribble_finance_app_design/theme/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:dribble_finance_app_design/theme/colors.dart';
+import 'package:dribble_finance_app_design/models/person.dart';
+import 'package:flutter/widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<List<Person>> futurePersons;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePersons = fetchPersons();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ThemeColors.backgroundColor,
+      backgroundColor: AppColors.background,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
         elevation: 0,
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: Icon(Icons.notifications_none),
-          )
+        backgroundColor: AppColors.background,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications_none, color: Colors.black),
+            onPressed: () {},
+          ),
         ],
       ),
-      body: SlidingUpPanel(
-        panel: Center(
-          child: Text("Sliding Widget"),
-        ),
-        body: const Row(
+      body: Padding(
+        padding: const EdgeInsets.all(0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: Column(
+              padding: const EdgeInsets.all(20),
+              child: const Text(
+                'Available balance',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 95, 92, 92),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: const Text(
+                '\$4820.23',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("Available balance"),
-                  Row(
-                    children: [
-                      Text("\$"),
-                      SizedBox(width: 2),
-                      Text("4820.23")
-                    ],
-                  )
+                  _buildActionButton('Pay', Icons.payment),
+                  _buildActionButton('Send', Icons.send),
+                  _buildActionButton('Request', Icons.request_page),
+                  _buildActionButton('Cash In', Icons.account_balance_wallet),
                 ],
               ),
-            )
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+  child: Container(
+    decoration: const BoxDecoration(
+      color: AppColors.bgWhite,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(25.0), // Sol üst köşe
+        topRight: Radius.circular(25.0), // Sağ üst köşe
+      ),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: FutureBuilder<List<Person>>(
+        future: futurePersons,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Failed to load persons'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No transactions found'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final transaction = snapshot.data![index];
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (index == 0 || snapshot.data![index - 1].amount.sign != transaction.amount.sign)
+                      Text(
+                        transaction.amount > 0 ? 'Received' : 'Sent',
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 16,
+                        ),
+                      ),
+                    _buildTransactionItem(transaction),
+                  ],
+                );
+              },
+            );
+          }
+        },
+      ),
+    ),
+  ),
+),
           ],
-        )
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Analytics',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionButton(String text, IconData icon) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 35,
+          backgroundColor: AppColors.accent,
+          child: Icon(icon, color: Colors.white),
+        ),
+        const SizedBox(height: 10),
+        Text(text),
+      ],
+    );
+  }
+
+  Widget _buildTransactionItem(Person transaction) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: ListTile(
+        leading: const CircleAvatar(
+          backgroundColor: Colors.grey,
+          child: Icon(Icons.person, color: Colors.white),
+        ),
+        title: Text(transaction.name),
+        trailing: Text(
+          transaction.amount.toStringAsFixed(2),
+          style: TextStyle(
+            color: transaction.amount > 0 ? Colors.green : Colors.red,
+            fontWeight: FontWeight.bold,
+            fontSize: 16
+          ),
+        ),
       ),
     );
   }
