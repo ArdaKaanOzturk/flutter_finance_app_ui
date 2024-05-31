85167bfd76e4c26b8aa5eaf5428aebc06abd20cb
ArdaKaanOzturk <ardakaan.ozturk@live.com>
2024-05-31 19:35:15 +0300

    home ui finalize

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -64,19 +64,46 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildActionButton('Pay', Icons.payment),
-                  _buildActionButton('Send', Icons.send),
-                  _buildActionButton('Request', Icons.request_page),
-                  _buildActionButton('Cash In', Icons.account_balance_wallet),
-                ],
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    _buildActionButton(
+                      icon: Icons.payment,
+                      label: 'Pay',
+                      onTap: () {
+                        // Handle Pay button tap
+                      },
+                    ),
+                    _buildActionButton(
+                      icon: Icons.send,
+                      label: 'Send',
+                      onTap: () {
+                        // Handle Send button tap
+                      },
+                    ),
+                    _buildActionButton(
+                      icon: Icons.request_page,
+                      label: 'Request',
+                      onTap: () {
+                        // Handle Request button tap
+                      },
+                    ),
+                    _buildActionButton(
+                      icon: Icons.money,
+                      label: 'Cash In',
+                      onTap: () {
+                        // Handle Cash In button tap
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
             const SizedBox(height: 20),
             Expanded(
   child: Container(
+    padding: const EdgeInsets.all(10),
     decoration: const BoxDecoration(
       color: AppColors.bgWhite,
       borderRadius: BorderRadius.only(
@@ -84,41 +111,47 @@ class _HomePageState extends State<HomePage> {
         topRight: Radius.circular(25.0), // Sağ üst köşe
       ),
     ),
-    child: Padding(
-      padding: const EdgeInsets.all(10),
-      child: FutureBuilder<List<Person>>(
-        future: futurePersons,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return const Center(child: Text('Failed to load persons'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No transactions found'));
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final transaction = snapshot.data![index];
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (index == 0 || snapshot.data![index - 1].amount.sign != transaction.amount.sign)
-                      Text(
-                        transaction.amount > 0 ? 'Received' : 'Sent',
-                        style: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
+    child: FutureBuilder<List<Person>>(
+      future: futurePersons,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Failed to load persons'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No transactions found'));
+        } else {
+          return ListView.builder(
+            itemCount: snapshot.data!.length + 1,
+            itemBuilder: (context, index) {
+              final transaction = snapshot.data![index];
+              return 
+              index == 0 ? 
+              Center(
+                child: Container(
+                  width: 60,
+                  height: 2,
+                  color: Colors.black,
+                ),
+              ) : 
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (index == 0 || snapshot.data![index - 1].amount.sign != transaction.amount.sign)
+                    Text(
+                      transaction.amount > 0 ? 'Received' : 'Sent',
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
                       ),
-                    _buildTransactionItem(transaction),
-                  ],
-                );
-              },
-            );
-          }
-        },
-      ),
+                    ),
+                  _buildTransactionItem(transaction),
+                ],
+              );
+            },
+          );
+        }
+      },
     ),
   ),
 ),
@@ -144,17 +177,43 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildActionButton(String text, IconData icon) {
-    return Column(
-      children: [
-        CircleAvatar(
-          radius: 35,
-          backgroundColor: AppColors.accent,
-          child: Icon(icon, color: Colors.white),
+  Widget _buildActionButton({required IconData icon, required String label, required VoidCallback onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 120,
+        width: 100,
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: AppColors.accent,
+          borderRadius: BorderRadius.circular(23),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              spreadRadius: 1,
+            ),
+          ],
         ),
-        const SizedBox(height: 10),
-        Text(text),
-      ],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon, size: 29, color: Colors.white),
+            const SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.only(top: 18),
+              child: Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
